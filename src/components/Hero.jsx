bf1e5e1beb32db6040b8e70616343c37a20ca92f
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-02-23 07:44:18 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
   return (
     <div className='hero_outer' >
         <div className='hero_text_div'>
-          <h1 data-aos='fade-down-right' data-aos-delay="400" data-aos-duration="1500">Hi There, Wellcom</h1>
+          <h1 data-aos='fade-down-right' data-aos-delay="400" data-aos-duration="1500">Hi There, Welcome</h1>
           <Typedtext />
           <button className='hire_me_btn' data-aos='fade-up-right' data-aos-delay="400" data-aos-duration="1500">Hire me </button> 
           <button className='portfolio_btn' data-aos='fade-up-left' data-aos-delay="400" data-aos-duration="1500">Portfolio</button>
