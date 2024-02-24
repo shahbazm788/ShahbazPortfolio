14f5b9cfa5fa11060b37e55b8b07a1fc121c2ece
Shahbaz Younus <52800069+shahbazm788@users.noreply.github.com>
2024-02-24 11:14:08 +0500

    Update Aboutcontent.jsx

--- src/components/Aboutcontent.jsx
+++ src/components/Aboutcontent.jsx
@@ -5,7 +5,7 @@ const Aboutcontent = () => {
   return (
     <div className='about_outer'>
       <Container>
-        {/* <h1 className='about_heading'>Are You need A "React" or "MERN" Developer ???</h1> */}
+        {/* <h1 className='about_heading'>Are You Need A "React" or "MERN" Developer ???</h1> */}
         <h1 className='about_heading'>Who Am I ?</h1>
         <p className='heading_border_p'></p>
         <Row>
@@ -13,7 +13,7 @@ const Aboutcontent = () => {
           <div className='about_text'>
             <p>Are You need A <span>"React"</span> or <span>"MERN"</span> Developer ???</p>
             
-            <p>If yes, Then You are on right place. I Am <span>Shahbaz</span> A certifide Mern stack developer with the couple of years experience of REACT and MERN developing You can finde my portfolio bleow. If you want to hire me you can contect me on any freelance plateform  like Freelance, Fiver, Upwork. or Find  me on Linkdin, facebook, X or Whatsapp etc. Thanks to reading </p>
+            <p>If yes, Then You are on the right place. I Am <span>Shahbaz</span> A certified Mern stack developer with a couple of years of experience REACT and MERN development You can find my portfolio below. If you want to hire me you can contect me on any freelance plateform  like Freelance, Fiver, Upwork. or Find  me on Linkdin, facebook, X or Whatsapp etc. Thanks to reading </p>
           </div>
           </Col>
         </Row>
